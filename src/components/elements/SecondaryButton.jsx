1456d012f1d18f3a6bfe2b40ba01c047074eb72f
Hossein Mohammadzadeh <4hmzjob@gmail.com>
2025-05-14 13:31:54 +0330

    fix:change header login butttn style

--- src/components/elements/SecondaryButton.jsx
+++ src/components/elements/SecondaryButton.jsx
@@ -1,6 +1,6 @@
 function SecondaryButton({ text }) {
   return (
-    <button className="w-fit border border-primary text-nowrap px-4 py-1.5 sm:py-2 rounded-lg text-primary hover:brightness-110 custom-transition cursor-pointer">
+    <button className="w-fit border border-primary text-nowrap px-4 py-1.5 sm:py-2 rounded-lg text-primary hover:brightness-90 custom-transition cursor-pointer">
       {text}
     </button>
   );
